c4d2077eb6e7e2f650a8aabfdc97989280e4fb5f
nopopcorn <157546145+nopopcorn@users.noreply.github.com>
2025-12-31 17:53:50 -0600

    Added more pages

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
+import AxyraBotPFP from "./images/AxyraBotPFP.png";
 import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: "AxyraBot",
   description: "AxyraBot – Smart Twitch bot for your channel",
+  icons: {
+    icon: AxyraBotPFP.src,
+  },
 };
 
 export default function RootLayout({
