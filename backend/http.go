5e28cb0c3fd9ab95f4dc882d314b71b27be01c2b
nopopcorn <157546145+nopopcorn@users.noreply.github.com>
2026-08-02 16:59:48 -0500

    Version 0.0.6 Alpha

--- backend/http.go
+++ backend/http.go
@@ -941,7 +941,7 @@ func withCORS(h http.HandlerFunc) http.HandlerFunc {
 				w.Header().Set("Vary", "Origin")
 			}
 		}
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
